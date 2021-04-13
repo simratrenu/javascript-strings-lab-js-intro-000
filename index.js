edea9e200c9993501de93ba42c02e599ce6686b6
simrat k renu <simratrenu@gmail.com>
2021-04-13 23:37:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Hello, {specialGuest!}";
 
-var greetSpecialGuest = "" specialGuest + "!";
+var greetSpecialGuest = "" specialGuest "!";
 
 var topic = "Let\'s talk about {topic}";
 
